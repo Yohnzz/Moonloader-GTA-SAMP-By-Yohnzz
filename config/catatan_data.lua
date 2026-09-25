83cf90604fc1e198ea972af488e21f900cb09050
yohnzz <ignasiusmario05@gmail.com>
2026-09-25 09:27:08 +0700

    Fix: gitignore

---
+++ config/catatan_data.lua
@@ -0,0 +1,11 @@
+return {
+  { cmd = "hargapolice", text = "» PEMBUATAN SKCK «\
+ • Pembuatan = Rp. 25O.OOO\
+\
+» SURAT KERAMAIAN «\
+ • Pembuatan = Rp. 30O.OOO\
+\
+» PEMBUATAN LISENSI SENJATA «\
+ • Pembuatan = Rp. 35O.OOO\
+ • Perpanjang = Rp. 20O.OOO" },
+}
---
+++ config/rp_floating_data.lua
@@ -0,0 +1,6 @@
+return {
+  { kategori = "BENGKEL KOTA OPEN", sub = {
+    { nama = "Buka Layanan", cmd = "/fa PENGUMUMAN | BENGKEL KOTA DIBUKA | MELAYANI REPAIR MODIF UP UP | TERIMAKASIH" },
+    { nama = "Tutup Layanan", cmd = "/fa PENGUMUMAN | BENGKEL KOTA TELAH DI TUTUP | TERIMAKASIH SEMUA PELANGGAN | ADIOS" },
+  }},
+}
